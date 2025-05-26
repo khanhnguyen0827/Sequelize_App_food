ef293a4c77acf4dc70a2cdd41f38407daa1497d2
Nguyễn Quốc Khánh <khanhnguyen0827@gmail.com>
2025-05-26 10:23:41 +0700

    base

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*************  ✨ Windsurf Command ⭐  *************/
+
 const express = require('express');
 const app = express();
 
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-/*******  e2e5435d-a859-4bb4-b344-30385e655a38  *******/
+
