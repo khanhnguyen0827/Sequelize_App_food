3a4c013b63b491c54b3f9a1416a0c84491f38669
Nguyễn Quốc Khánh <68071460+khanhnguyen0827@users.noreply.github.com>
2025-06-06 21:56:45 +0700

    expess

--- src/routes/orders.routes.js
+++
@@ -1,9 +0,0 @@
-// Định nghĩa các tuyến đường (routes) cho chức năng Order
-import express from 'express';
-const router = express.Router();
-import { createOrder, getOrdersByUser, getOrderById } from '../controllers/orders.Controller.js';
-
-router.post('/', createOrder);
-router.get('/user/:userId', getOrdersByUser);
-router.get('/:orderId', getOrderById);
-export default router;
--- src/routes/ratings.routes.js
+++
@@ -1,9 +0,0 @@
-// Định nghĩa các tuyến đường (routes) cho chức năng Rating
-import express from 'express';
-const router = express.Router();
-import { upsertRating, getRatingsByRestaurant, getRatingsByUser } from '../controllers/ratings.Controller.js';
-
-router.post('/', upsertRating);
-router.get('/restaurant/:restaurantId', getRatingsByRestaurant);
-router.get('/user/:userId', getRatingsByUser);
-export default router;
